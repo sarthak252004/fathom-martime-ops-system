560a393646c98fc420445f6e8acc2d55c1cf68d6
sarthak252004 <137911376+sarthak252004@users.noreply.github.com>
2026-06-23 01:30:31 +0530

    Use environment variable for DATABASE_URL

--- fastapi-crash/database.py
+++ fastapi-crash/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-DATABASE_URL = "postgresql://sarthak:sarthak252004@localhost:5432/fathom"
+DATABASE_URL = os.getenv("DATABASE_URL",postgresql://sarthak:sarthak252004@localhost:5432/fathom")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
